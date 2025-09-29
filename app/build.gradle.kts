fed2124d18dd19bec529ad8321557d4b69724dfe
marjunstudio <uebujie@gmail.com>
2025-09-29 23:49:06 +0900

    不要なファイルを追跡から削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.actresssearchapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
